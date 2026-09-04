5c429057f5694941f5f3e7239a56a8d243cb8dd6
Sqiboo <goelbalasanyan@gmail.com>
2026-09-04 18:05:53 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -1,14 +1,14 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Goel Balasanyan
+ *  Date: 9-2-26
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+        String args = new string("Happy Birthday, to you")
+		System.out.print("Happy Birthday,
+		
 	}
 }
