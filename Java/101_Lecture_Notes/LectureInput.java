58179de731d31e9bfde521b4989048c9fb465df2
Sqiboo <goelbalasanyan@gmail.com>
2026-09-15 18:25:05 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,25 @@
-/*
-    Lecture note example - Input!!
-*/
+import java.util.Scanner;
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
-	}
+        Scanner sc = new Scanner(System.in);
+        // our Code Goes here
+    System.out.println("Welcome to Goel's Goodies!"); 
+	System.out.println("Here's a list of our goodies:");
+    
+    double item1Price = 2.99;
+    System.out.println("Sour Patch Kids - $" + item1Price);
+    System.out.println("item1Price"); 
+
+    double item2Price = 0.99;
+    System.out.println("Chester's Hot Fries - $" + item2Price);
+
+    System.out.println("Can of Coke - $" + item3Price);
+    
+    double item3Price = 1.99;
+    System.out.println("item3Price");
+    
+
+
+    }
 }
