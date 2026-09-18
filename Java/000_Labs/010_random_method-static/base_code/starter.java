530c895cb641e5c9010ac51fec58051080d38852
Sqiboo <goelbalasanyan@gmail.com>
2026-09-18 17:55:51 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -10,6 +10,8 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("A number between 0 and 9:" + (int)(Math.random() * 10));
+		System.out.println("A number between 1 and 10:" + (int)(Math.random() * 10 + 1));
+		System.out.println("A number between 2.5 and 3.5:" + (Math.random() * 1 + 2.5));
 	}
 }
