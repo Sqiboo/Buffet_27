f465f4e1ba622cdc09e56033e5b276d130129e11
Sqiboo <goelbalasanyan@gmail.com>
2026-09-23 18:09:17 +0000

    update

--- Java/102_Exercises/12_Colors/base_code/starter.java
+++ Java/102_Exercises/12_Colors/base_code/starter.java
@@ -8,7 +8,19 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		
+        int red = (int)(Math.random() * 256);
+        int green = (int)(Math.random() * 256);
+        int blue = (int)(Math.random() * 256);
+        System.out.println("Random Color: rgb(" + red + ", " + green + ", " + blue + ")");
+        System.out.println("Complementary Color: rgb(" + (255-red) + ", " + (255-green) + ", " + (255-blue) + ")");
+        getColor(red, green, blue);
+        getColor(255-red, 255-green, 255-blue);
+        System.out.println("Triadic Colors ");
+        getColor(red, green, blue);
+        getColor(blue, green, red);
+        getColor(green, red, blue);
+        System.out.println("Dark Color: rgb(" + (127-red) + ", " + (127-green) + ", " + (127-blue) + ")");
+        getColor(red-127, green-127, blue-127);
 
 		// Call getColor(#, #, #);
 	}
